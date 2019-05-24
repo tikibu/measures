464a82130f7fb32d89d907a9d76538a3c11d640c
Igor Gervasiychuk <ig@otm-r.com>
2019-05-24 10:05:50 +0300

    Changed default pool settings

--- redis_incrementer.go
+++ redis_incrementer.go
@@ -20,8 +20,9 @@ func NewRedisIncrementer(host string, port int) (fi *RedisIncrementer) {
 
 func dialRedis(host string) *redis.Pool {
 	return &redis.Pool{
-		MaxIdle:     5,
-		IdleTimeout: 0,
+		MaxIdle:     1000,
+		MaxActive: 	 2000,
+		IdleTimeout: 240 * time.Second,
 		Dial: func() (redis.Conn, error) {
 			c, err := redis.Dial(
 				"tcp",
